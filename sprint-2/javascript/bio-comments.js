fc618b01273afe3ce8030d81cc110691e50437e7
Shelby Mitchell <shelbzmitchell@gmail.com>
2020-01-31 17:49:53 -0800

    javascript structure sorted for bio and shows

--- sprint-2/javascript/bio-comments.js
+++ sprint-2/javascript/bio-comments.js
@@ -20,76 +20,69 @@ let commentsArr = [
   }
 ];
 
-let infoContainer = document.querySelector(".comments"); //selecting div in html and creating variable named infoContainer
+console.log(commentsArr);
 
-function createCommentSection(infoContainer, oldComments) {
+let infoContainer = document.querySelector(".comments__displayed"); //selecting div in html and creating variable named infoContainer
+
+function displayComment(commentChunk) {
   //declaring function named createCommentSection, parameters are the div (var infoContainer and container for shows array (showInfo))
 
-  for (let commentChunk of oldComments) {
-    //go through array one by one--commentChunk represents each object within array, oldComments is array itself -- used of for objects
+  //   for (let commentChunk of commentObject) {
+  //go through array one by one--commentChunk represents each object within array, oldComments is array itself -- used of for objects
 
-    let oldCommentsContainer = document.createElement("div"); //variable within for loop of each object - div for each section
-    oldCommentsContainer.classList.add("comment__container");
-    infoContainer.appendChild(oldCommentsContainer); //append div for each comment chunk to the parrent div infoContainer
-    let commentsEl = document.createElement("div"); //create div for each key/value pair, 3 separate for different key values
-    let commentsEl2 = document.createElement("div");
-    let commentsEl3 = document.createElement("div");
-    for (let key in commentChunk) {
-      //goes through each object key within each item
+  // let commentsBlock = document.createElement("div"); //variable within for loop of each object - div for each section
+  // commentsBlock.classList.add("comment__container");
+  // infoContainer.appendChild(commentsBlock); //append div for each comment chunk to the parrent div infoContainer
+  for (let key in commentChunk) {
+    //goes through each object key within each item
 
-      if (key === "name") {
-        console.log(commentChunk["name"]);
-        let nameEl = document.createElement("p"); //create p tag variable nameEl
-        nameEl.innerText = `${key} ${commentChunk["name"]}`; //nameEl will show the info if key matches what's declared
-        nameEl.className = "comments__name";
-        commentsEl.appendChild(nameEl); //append to specified div
-      }
+    let commentsEl = document.createElement("div"); //variable within for loop of each object - div for each section
+    commentsEl.classList.add("comment__container");
+    infoContainer.appendChild(commentsEl); //append div for each comment chunk to the parrent div infoContainer
 
-      if (key === "comment") {
-        console.log(commentChunk["comment"]);
-        let commentEl = document.createElement("p");
-        commentEl.innerText = `${key} ${commentChunk["comment"]}`;
-        commentEl.className = "comments__comment";
-        commentsEl2.appendChild(commentEl);
-      }
-
-      if (key === "date") {
-        console.log(commentChunk["date"]);
-        let dateEl = document.createElement("p");
-        dateEl.innerText = `${key} ${commentChunk["date"]}`;
-        dateEl.className = "comments__date";
-        commentsEl3.appendChild(dateEl);
-      }
+    if (key === "name") {
+      console.log(commentChunk["name"]);
+      let nameEl = document.createElement("p"); //create p tag variable nameEl
+      nameEl.innerText = `${key} ${commentChunk["name"]}`; //nameEl will show the info if key matches what's declared
+      nameEl.className = "comments__name";
+      commentsEl.appendChild(nameEl); //append to specified div
     }
 
-    oldCommentsContainer.appendChild(commentsEl); //append div to container for all comment chunks
-    commentsEl.classList.add("comments__el-container");
-    oldCommentsContainer.appendChild(commentsEl2);
-    commentsEl2.classList.add("comments__el-container");
-    oldCommentsContainer.appendChild(commentsEl3);
-    commentsEl3.classList.add("comments__el-container");
+    if (key === "comment") {
+      console.log(commentChunk["comment"]);
+      let commentEl = document.createElement("p");
+      commentEl.innerText = `${key} ${commentChunk["comment"]}`;
+      commentEl.className = "comments__comment";
+      commentsEl.appendChild(commentEl);
+    }
+
+    if (key === "date") {
+      console.log(commentChunk["date"]);
+      let dateEl = document.createElement("p");
+      dateEl.innerText = `${key} ${commentChunk["date"]}`;
+      dateEl.className = "comments__date";
+      commentsEl.appendChild(dateEl);
+    }
   }
+
+  // oldCommentsContainer.appendChild(commentsEl); //append div to container for all comment chunks
+  // commentsEl.classList.add("comments__el-container");
+  // oldCommentsContainer.appendChild(commentsEl2);
+  // commentsEl2.classList.add("comments__el-container");
+  // oldCommentsContainer.appendChild(commentsEl3);
+  // commentsEl3.classList.add("comments__el-container");
 }
 
 //invoke the createCommentSection function
-createCommentSection(infoContainer, commentsArr); //arguments
+for (let object of commentsArr) displayComment(object);
 
-const formSection = document.querySelector(".comments__form"); //add header above form, in same parent div as form
-const header = document.createElement("h1");
-header.className = "comments__header";
-header.innerText = "Join the Conversation";
-formSection.appendChild(header);
+// const formSection = document.querySelector(".comments__form"); //add header above form, in same parent div as form
+// const header = document.createElement("h1");
+// header.className = "comments__header";
+// header.innerText = "Join the Conversation";
+// formSection.appendChild(header);
 
 //
-
-function displayComment(userName, userComment) {
-  let newComment = {
-    name: userName,
-    comment: userComment,
-    date: new Date()
-  };
-  console.log(newComment);
-}
 
 const form = document.querySelector(".comments__form");
 
@@ -97,7 +90,14 @@ form.addEventListener("submit", submitEvent => {
   //higher order function - when listener is activated, will execute function inside (inner function is parameter of this)
   submitEvent.preventDefault();
 
-  let userName1 = document.querySelector(".comments__new-name").value;
-  let userComment1 = document.querySelector(".comments__new-comment").value;
-  displayComment(userName1, userComment1);
+  let userName = document.querySelector(".comments__new-name").value;
+  let userComment = document.querySelector(".comments__new-comment").value;
+  let newComment = {
+    name: userName,
+    comment: userComment,
+    date: new Date()
+  };
+  commentsArr.unshift(newComment);
+  document.querySelector(".comments__displayed").innerText = "";
+  for (let object of commentsArr) displayComment(object);
 });
